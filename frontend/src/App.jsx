5cb12483caf87d23b66622e092b607becdfef61e
DanielSolarte23 <royersolarte22@gmail.com>
2024-10-04 18:22:50 -0500

    Tareas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import NavBar from "./components/NavBar";
+import NuevaTarea from "./components/NuevaTarea";
 
 export default function App() {
   return (
@@ -9,7 +10,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={Home} />
         <Route path="/mostrar" element={<h1>Hola Mundo</h1>} />
-        <Route path="/agregar" element={<h1>Agregar Tarea</h1>} />
+        <Route path="/agregar" element={<NuevaTarea/>} />
       </Routes>
     </Router>
   );
--- frontend/src/components/NuevaTarea.jsx
+++ frontend/src/components/NuevaTarea.jsx
@@ -2,8 +2,11 @@ export default function NuevaTarea() {
   return (
     <>
     <form action="">
-        
-
+        <div className="border border-red-600 flex flex-col justify-center px-32 gap-5">
+            <input className="border px-5 py-5" type="text" placeholder="Titulo"/>
+            <textarea className="border px-5" name="descripcion" id="" placeholder="Descripcion"></textarea>
+            <button className="bg-lime-600 text-white">Agregar Tarea</button>
+        </div>
     </form>
     </>
   )
